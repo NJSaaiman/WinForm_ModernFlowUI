86041b0824df1fb1be4bd98682eba788e2ba4a62
NJSaaiman <njsaaiman@gmail.com>
2016-03-24 17:37:14 +0200

    test

--- ModernUI.Test/Master.cs
+++ ModernUI.Test/Master.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using ModernUI.Structures.Core.Forms.MessageBoxes;
+using ModernUI.Structures.Style;
 
 namespace ModernUI.Test
 {
@@ -16,17 +17,21 @@ namespace ModernUI.Test
 
         void Master_Load(object sender, System.EventArgs e)
         {
+            IStyleManager manager = new DarkStyle() { BackColor = Color.Aqua, ForeColor = Color.PaleGreen, MdiParentBackColor = Color.Olive, TitleBarBackColor = Color.Yellow, TitleBarForeColor = Color.Black };
             Form1 frm = new Form1();
+            frm.StyleManager = manager;
             frm.MdiParent = this;
             //frm.Dock = System.Windows.Forms.DockStyle.Left;
             frm.Show();
 
             MUIMessageBox msgb = new MUIMessageBox(System.Windows.Forms.MessageBoxButtons.YesNo);
+            msgb.StyleManager = manager;
             
             msgb.Text = "messgae box";
             if (msgb.Show() == System.Windows.Forms.DialogResult.Yes)
             {
                 Form1 frm2 = new Form1();
+                frm2.StyleManager = manager;
                 frm2.MdiParent = this;
                 frm2.Dock = System.Windows.Forms.DockStyle.Bottom;
                 frm2.Show();
